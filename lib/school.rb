1cae10ae627779e5fea0303e06758951ccf5c5ee
AfraaAlobaid <afraa.obaid1@gmail.com>
2022-01-15 21:46:15 +1030

    Used instance methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+    attr_accessor :roster, :name
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if  @roster[grade]
+            @roster[grade]  << name
+        else 
+            @roster[grade] = [name]
+        end
+    end
+
+    def grade(num)
+        @roster[num]
+    end
+
+    def sort
+        #@roster.sort
+        @roster.each do |key, value|
+            value.sort!
+        end
+        @roster
+    end
+end
